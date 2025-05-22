48fda47e13d384d3c022a2b975efe7077ced8439
Bhawana <bhwbi.rawal@gmail.com>
2025-05-22 11:51:58 +0545

    Add project files.

---
+++ BookBay/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookBay.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
