130950c68db73bbbd8fe34ba510d09b379444699
Bhawana <bhwbi.rawal@gmail.com>
2025-06-25 13:36:24 +0545

    add create module

---
+++ BookBay/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using BookBay.Data;
+using BookBay.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookBay.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategorylist= _db.Categories.ToList();
+            return View(objCategorylist);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+             
+        }
+
+    }
+}
---
+++ BookBay/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BookBay.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookBay.Data
+{
+    public class ApplicationDbContext:DbContext     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        // Define DbSets for your entities here, e.g.:
+        public DbSet<Category> Categories { get; set; }
+        // public DbSet<User> Users { get; set; }
+        // public DbSet<Order> Orders { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                  new Category { Id = 3, Name = "History ", DisplayOrder = 3 });
+                 // Assuming Category has an Id property
+            base.OnModelCreating(modelBuilder);
+            // Configure entity relationships and properties here if needed
+        }
+    }
+    
+}
